fff1967b85a0e91d54e4204584845ef2aaa9904e
erastov-alex <erastov.alex@gmail.com>
2023-09-11 22:57:34 +0300

    update ABSTRACT.py, settings.py

--- src/settings.py
+++ src/settings.py
@@ -34,7 +34,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://github.com/ishann/aeroscapes"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 3550664
+PREVIEW_IMAGE_ID: int = 3548236
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/aeroscapes"
