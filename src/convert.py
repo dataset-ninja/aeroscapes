6cb804bc5509af8d6b4f72dbd835300cc35b2859
erastov-alex <erastov.alex@gmail.com>
2023-09-11 14:11:24 +0300

    update convert.py, settings.py, gitignore

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,14 @@
-import supervisely as sly
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
-import shutil
 
+import numpy as np
+import supervisely as sly
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import file_exists, get_file_name
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -52,7 +54,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -60,21 +63,111 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    # raise NotImplementedError("The converter should be implemented manually.")
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    project_name = "Aeroscapes"
+    dataset_path = "aeroscapes"
+    img_path = os.path.join(dataset_path, "JPEGImages")
+    segmentation_path = os.path.join(dataset_path, "Visualizations")
+    batch_size = 50
 
-    # ... some code here ...
+    classes = {
+        "Background": (0, 0, 0),
+        "Person": (192, 128, 128),
+        "Bike": (0, 128, 0),
+        "Car": (128, 128, 128),
+        "Drone": (128, 0, 0),
+        "Boat": (0, 0, 128),
+        "Animal": (192, 0, 128),
+        "Obstacle": (192, 0, 0),
+        "Construction": (192, 128, 0),
+        "Vegetation": (0, 64, 0),
+        "Road": (128, 128, 0),
+        "Sky": (0, 128, 128),
+    }
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+    train_set = os.path.join(dataset_path, "ImageSets", "trn.txt")
+    val_set = os.path.join(dataset_path, "ImageSets", "val.txt")
 
-    # return project
+    project_dict = {"val": val_set, "train": train_set}
 
+    for ds in project_dict:
+        with open(project_dict[ds]) as f:
+            lines = f.readlines()
+            images = [line[: line.find("\n")] + ".jpg" for line in lines]
+            project_dict[ds] = images
 
+    def get_unique_colors(img):
+        unique_colors = []
+        img = img.astype(np.int32)
+        h, w = img.shape[:2]
+        colhash = img[:, :, 0] * 256 * 256 + img[:, :, 1] * 256 + img[:, :, 2]
+        unq, unq_inv, unq_cnt = np.unique(colhash, return_inverse=True, return_counts=True)
+        indxs = np.split(np.argsort(unq_inv), np.cumsum(unq_cnt[:-1]))
+        col2indx = {unq[i]: indxs[i][0] for i in range(len(unq))}
+        for col, indx in col2indx.items():
+            if col != 0:
+                unique_colors.append((col // (256**2), (col // 256) % 256, col % 256))
+        unique_colors.append((0, 0, 0))
+        return unique_colors
+
+    def get_key(dict, value):
+        for k, v in dict.items():
+            if v == value:
+                return k
+
+    def create_ann(image_path):
+        labels = []
+        image_name = get_file_name(os.path.basename(image_path))
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
+        mask_path = os.path.join(segmentation_path, f"{image_name}.png")
+        if file_exists(mask_path):
+            mask_np = sly.imaging.image.read(mask_path)
+            if len(np.unique(mask_np)) != 1:
+                uniq_color = get_unique_colors(mask_np)
+                for color in uniq_color:
+                    obj_class = meta.get_obj_class(get_key(classes, color))
+                    mask = np.all(mask_np == color, axis=2)
+                    curr_bitmap = sly.Bitmap(mask)
+                    curr_label = sly.Label(curr_bitmap, obj_class)
+                    labels.append(curr_label)
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+    obj_classes = [
+        sly.ObjClass(label_name, sly.Bitmap, classes[label_name]) for label_name in classes
+    ]
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(obj_classes=obj_classes)
+    api.project.update_meta(project.id, meta.to_json())
+
+    dataset = dataset_val = api.dataset.create(project.id, "val", change_name_if_conflict=True)
+    dataset_train = api.dataset.create(project.id, "train", change_name_if_conflict=True)
+
+    progress = sly.Progress(
+        "Create dataset {}".format(dataset.name),
+        len(project_dict["train"]) + len(project_dict["val"]),
+    )
+
+    for ds in project_dict:
+        dataset = dataset_train if ds == "train" else dataset_val
+        image_names = project_dict[ds]
+        for img_names_batch in sly.batched(image_names, batch_size=batch_size):
+            img_pathes_batch = [os.path.join(img_path, img_name) for img_name in img_names_batch]
+            img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+            img_ids = [im_info.id for im_info in img_infos]
+            anns_batch = [create_ann(image_path) for image_path in img_pathes_batch]
+            api.annotation.upload_anns(img_ids, anns_batch)
+            progress.iters_done_report(len(img_names_batch))
+
+            # sly.logger.info('Deleting temporary app storage files...')
+            # shutil.rmtree(storage_dir)
+
+    return project
